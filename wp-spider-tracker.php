28d5de21d603484af632aa72e4c36eced755070b
Eugen <pustojack@list.ru>
2020-11-11 09:52:01 +0200

    Add files via upload

--- wp-spider-tracker.php
+++ wp-spider-tracker.php
@@ -21,31 +21,29 @@ if (!defined('ABSPATH')) die("'tsk-tsk' You should only access this page via WP-
 
 //###  VARIABLES  ###//
 global $wpdb, $table_name, $wpstp_db_version, $wpstp_version, $wpstp_perms, $WPSTP_PATH;
-$table_name = $wpdb->prefix . "mnm_spider_tracker";
+$table_name = $wpdb->prefix . "wpstp_spider_tracker";
 $wpstp_db_version = "1.0";
 $wpstp_version = "1.0.3";
 $wpstp_perms = 10;	 // http://codex.wordpress.org/User_Levels
-$WPSTP_PATH = WP_PLUGIN_URL."/wp-spider-tracker";
+$WPSTP_PATH = plugins_url('wp-spider-tracker');
 
 
 //###  HOOKS  ###//
 register_activation_hook( __FILE__, 'wpstp_install' );
 add_action( 'wp_head', 'wpstp_is_bot' );
 add_action( 'admin_menu', 'wpstp_admin_block' );
-add_action( 'admin_head', 'wpstp_admin_head' );
+
 
 
 //###  ADMINISTRATION  ###//
-function wpstp_admin_head() {
-	global $WPSTP_PATH;
-	// load CSS and SCRIPT pages to the admin head
-    echo '
-		<!-- Start of MnM Spider Tracker includes -->
-		<link href="'. $WPSTP_PATH .'/wpstp_styles.css" rel="stylesheet" type="text/css" media="screen, projection" />
-		<!-- End of MnM Spider Tracker includes -->
 
-';
-}
+	// load CSS and SCRIPT pages to the admin head 
+add_action( 'admin_enqueue_scripts', function () {
+wp_enqueue_style( 'wpstp-admin-style', $WPSTP_PATH .'/wpstp_styles.css' );
+});
+
+
+
 
 function wpstp_admin_block() {
 	//Include menus
